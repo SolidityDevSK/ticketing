1c3a295efeb776527bb3ed2e3f8c29f742c78860
SolidityDevSK <serdarkayaci89@gmail.com>
2023-10-29 01:37:36 +0300

    thirdly commit

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import expres from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 import {
   currentUserRouter,
   signinRouter,
@@ -14,6 +15,13 @@ import { NotFoundError } from "./errors/not-found-error";
 const app = expres();
 
 app.use(json());
+app.set("trust proxy", true);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
