c7b39ee648e6ee20dd7478287daafbba030e269b
SolidityDevSK <serdarkayaci89@gmail.com>
2023-11-07 21:40:36 +0300

    new commit

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { ValidateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { Password } from "../services/password";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -14,7 +17,21 @@ router.post(
       .withMessage("You must supply a password"),
   ],
   ValidateRequest,
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) throw new BadRequestError("Invalid credentials");
+    return res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
